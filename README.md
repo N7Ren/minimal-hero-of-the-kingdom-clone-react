0ff30cb114de23af04681e1261e804d3d6da42ee
RemoteRen <ren@remoteren.com>
2020-02-10 09:41:02 +0100

    first version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Minimal "Hero of The Kingdom" Clone in React
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/index.js
+++ src/index.js
@@ -1,12 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Countdown, {zeroPad} from 'react-countdown';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  
+  class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: [null, 'M', null, null, null, null, null, null, 'M'],
+        inventory: {
+          "Mushrooms": 0,
+        },
+        date: Date.now() + 5000,
+      };
+    }
+  
+    handleClick(i) {
+      if(this.state.squares[i] === 'M') {
+        this.state.inventory["Mushrooms"]++;
+      }
+      this.state.squares[i] = null;
+      this.setState({squares: this.state.squares, inventory: this.state.inventory});
+    }
+  
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    countDownRenderer = ({ minutes, seconds, completed }) => {
+      if (completed) { //reset
+        this.state.squares = [null, 'M', null, null, null, null, null, null, 'M'];
+        this.setState({ squares: this.state.squares, date: Date.now() + 5000 });
+      }
+    
+      return (
+        <span>
+          {zeroPad(minutes)}:{zeroPad(seconds)}
+        </span>
+      );
+    };
+  
+    render() {
+      const status = 'Player inventory: Mushrooms:' + this.state.inventory["Mushrooms"];
+
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <br/>
+          <div>
+            Time to respawn: <Countdown 
+                              key={this.state.date} 
+                              date={this.state.date} 
+                              renderer={this.countDownRenderer}
+                            />
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
